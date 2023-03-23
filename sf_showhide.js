024ed9f53afd4fdfb5de64ac554dcb66cf3ea641
Ehtemami <ehtemami.anahid@mayo.edu>
2023-03-23 11:44:55 -0500

    updated model

--- sf_showhide.js
+++ sf_showhide.js
@@ -1,6 +1,6 @@
 // Sketchfab Viewer API: Start/Stop the viewer
 var version = "1.9.0";
-var uid = "784e95f4f22545199be7e165af6437f8";
+var uid = "a494f0d7b92745689dd83aec13d2e545";
 
 var urlParams = new URLSearchParams(window.location.search);
 var autoSpin = 0.0;
